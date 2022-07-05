6bd4a07f48a83523f552741ace15ff4af6633c43
Dong487 <Dsheng7777777@gmail.com>
2022-07-06 05:55:30 +0800

    Initial Commit

---
+++ Peter013_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Peter013_1
+//
+//  Created by DONG SHENG on 2022/7/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peter013_1/Peter013_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Peter013_1App.swift
+//  Peter013_1
+//
+//  Created by DONG SHENG on 2022/7/6.
+//
+
+import SwiftUI
+
+@main
+struct Peter013_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
